1a9dbe649a9adcc8cc30a3724085a0c82e715b45
nimrod46 <nimrodma99@gmail.com>
2022-06-20 12:33:46 +0300

    Added lab13

--- src/Lab13/Calculator.java
+++ src/Lab13/Calculator.java
@@ -1,9 +1,11 @@
-package Lab12;
+package Lab13;
 
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class Calculator extends JFrame {
 
@@ -16,7 +18,8 @@ public class Calculator extends JFrame {
 
     public Calculator() {
         this.getContentPane().setLayout(new BorderLayout());
-        calculatorForm();
+
+        createCalculatorForm();
 
         JPanel northPanel = new JPanel();
         northPanel.setPreferredSize(new Dimension(200, 20));
@@ -30,7 +33,10 @@ public class Calculator extends JFrame {
         southPanel.setPreferredSize(new Dimension(200, 50));
         add(southPanel, BorderLayout.SOUTH);
         exitButton = new JButton("Exit");
-        southPanel.setLayout(new FlowLayout(FlowLayout.RIGHT, 50, 20));
+        exitButton.addActionListener(e -> {
+            System.exit(1);
+        });
+        southPanel.setLayout(new FlowLayout(FlowLayout.RIGHT, 50,20));
         southPanel.add(exitButton, BorderLayout.EAST);
 
         JPanel eastPanel = new JPanel();
@@ -43,17 +49,17 @@ public class Calculator extends JFrame {
         repaint();
     }
 
-    private void calculatorForm() {
+    private void createCalculatorForm() {
         JPanel jPanel = new JPanel();
         GridLayout gridLayout = new GridLayout(4, 2);
         gridLayout.setVgap(5);
         gridLayout.setHgap(5);
         jPanel.setLayout(gridLayout);
-        jPanel.setPreferredSize(new Dimension(350, 180));
-        jPanel.setMaximumSize(new Dimension(350, 180));
+        jPanel.setPreferredSize(new Dimension(350,180));
+        jPanel.setMaximumSize(new Dimension(350,180));
         jPanel.setBorder(BorderFactory.createCompoundBorder(
                 BorderFactory.createLineBorder(Color.red, 5), // outer border
-                BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+                BorderFactory.createEmptyBorder(5,5,5,5)));
 
         formInput(jPanel);
         formInteraction(jPanel);
@@ -70,6 +76,7 @@ public class Calculator extends JFrame {
 
         secondNumberInput = new JTextField();
         secondNumberInput.setBorder(inputBorder);
+        secondNumberInput.addActionListener(this::addNumbersPressed);
 
         resultOutput = new JTextField();
         resultOutput.setBorder(inputBorder);
@@ -86,11 +93,29 @@ public class Calculator extends JFrame {
     private void formInteraction(JPanel jPanel) {
         JPanel addClearPanel = new JPanel();
         add = new JButton("Add");
-
+        add.addActionListener(this::addNumbersPressed);
         clear = new JButton("Clear");
+        clear.addActionListener(this::clearButtonPressed);
         addClearPanel.add(add);
         addClearPanel.add(clear);
         jPanel.add(new JPanel());
         jPanel.add(addClearPanel);
+    }
+
+    private void clearButtonPressed(ActionEvent e) {
+        firstNumberInput.setText("");
+        secondNumberInput.setText("");
+        resultOutput.setText("");
+    }
+
+    private void addNumbersPressed(ActionEvent e) {
+        try {
+            int firstN = Integer.parseInt(firstNumberInput.getText());
+            int secondN = Integer.parseInt(secondNumberInput.getText());
+            resultOutput.setText(firstN + secondN + "");
+        } catch (Exception ex) {
+            JOptionPane jOptionPane = new JOptionPane("Not a number!", JOptionPane.ERROR_MESSAGE, JOptionPane.DEFAULT_OPTION);
+            jOptionPane.createDialog("Error!").setVisible(true);
+        }
     }
 }
---
+++ src/Lab13/Lab13Test.java
@@ -0,0 +1,12 @@
+package Lab13;
+
+public class Lab13Test {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+    }
+
+
+
+
+}
